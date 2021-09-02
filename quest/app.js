c351c47c14e7bd83582fb64a8f7f8d17335664df
yosefkirkcohen <ycohen45@yahoo.com>
2021-09-02 15:45:40 -0700

    choices rendering

--- quest/app.js
+++ quest/app.js
@@ -15,9 +15,27 @@ questImage.src = `../assets/${questObj.image}`;
 const description = document.createElement('p');
 description.textContent = questObj.description;
 
+const form = document.createElement('form');
+
+
+for (let choice of questObj.choices) {
+  const currentChoice = document.createElement('p');
+  currentChoice.textContent = choice.description;
+  
+  const radioButton = document.createElement('input')
+  radioButton.type = 'radio';
+
+  const label = document.createElement('label');
+  label.append(currentChoice, radioButton)
+  form.append(label);
+}
+
+
+
 
 
 
 const questDiv = document.getElementById('quest-div')
 questDiv.append(questImage);
 questDiv.append(description);
+questDiv.append(form);
