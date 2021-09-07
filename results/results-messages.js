a4a3000187c16de6109cef6b81df9ae6593dd834
yosefkirkcohen <ycohen45@yahoo.com>
2021-09-07 15:00:18 -0700

    making messages

---
+++ results/results-messages.js
@@ -0,0 +1,18 @@
+export const moneyAliveMessage = {
+    debt: 'Crippling debt makes you consider suicide daily.',
+    poor: 'You are indigent, but at least your family loves you.',
+    modest: 'You have enough money to take someone out for a steak dinner tonight, but no one to take out.',
+    rich: 'You\'re loaded. Nice! You buy a haunted mansion as a summer home where you hang out with the ghost Anthony Bourdain'
+};
+
+export const moneyDeadMessage = {
+    poor: 
+    modest: 
+    rich: 
+};
+
+export const healthMessage = {
+    dead: 'Few can withstand 3 deaths and walk away. You are not the few. You are dead.',
+    unhealthy: 'This multiple lives business has taken its toll. You can\'t climb like you used to. You should stick to baking.', 
+    healthy: 'Not spending any energy on resurrection has left you feeling like you could climb a mountain!'
+};
